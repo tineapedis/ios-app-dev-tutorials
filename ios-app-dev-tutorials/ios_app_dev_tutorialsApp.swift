de1d369f59ae7721522e13dee981cdcdf7d818e7
tineapedis <nmurata73@gmail.com>
2022-08-19 15:12:01 +0900

    Initial Commit

---
+++ ios-app-dev-tutorials/ios_app_dev_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_dev_tutorialsApp.swift
+//  ios-app-dev-tutorials
+//
+//  Created by nmurata on 2022/08/19.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_dev_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
